29d88a84e8e81953a4a3c1bb5814dbd600439d7c
Ky Ky <kyky@kyky.ky>
2021-05-17 16:17:10 +0300

    *

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,14 @@ import { AppInitialQuery } from "__relay__/AppInitialQuery.graphql";
 import { AppComposersQuery } from "__relay__/AppComposersQuery.graphql";
 import { IEnvironment } from "relay-runtime";
 
-type CountriesEnum = AppInitialQuery["variables"]["country"];
-type WorkKindEnum = AppInitialQuery["variables"]["workKind"];
+function isNully(value: any) {
+  return value === null || value === undefined;
+}
+
+function removeUndefinedValues(ob: Object) {
+  let pairs = Object.entries(ob).filter((pair) => pair[1] !== undefined);
+  return Object.fromEntries(pairs);
+}
 
 export const InitialQuery = graphql`
   query AppInitialQuery($country: Country, $workKind: WorkKind) {
@@ -67,114 +73,130 @@ function ComposersList({
 
 function App(props: { initialQueryRef: PreloadedQuery<AppInitialQuery> }) {
   const [state, setState] = useState<{
+    // null or undefined means value is not set
     current: AppComposersQuery["variables"];
-    draft: AppComposersQuery["variables"];
+    // undefined means value is not set
+    // null means value is unset
+    draftDelta: AppComposersQuery["variables"];
   }>({
     current: {
       country: props.initialQueryRef.variables.country,
       workKind: props.initialQueryRef.variables.workKind,
     },
-    draft: {},
+    draftDelta: {},
   });
   const data = usePreloadedQuery(InitialQuery, props.initialQueryRef);
   const [composersQueryRef, reloadComposersQuery] =
     useQueryLoader<AppComposersQuery>(ComposersQuery);
 
-  const { countryValues, workKindValues, composers } = data;
-
   function handleApply() {
     const nextState = {
-      current: { ...state.current, ...state.draft },
-      draft: {},
+      current: { ...state.current, ...removeUndefinedValues(state.draftDelta) },
+      draftDelta: {},
     };
     setState(nextState);
     reloadComposersQuery(nextState.current);
   }
   function handleCancel() {
     setState((prev) => {
-      prev.draft = {};
-      return prev;
+      const next = { ...prev };
+      next.draftDelta = {};
+      return next;
     });
   }
 
   const onChange =
     (param: keyof AppComposersQuery["variables"]) =>
     (evt: React.ChangeEvent<HTMLSelectElement>) => {
-      let nextValue = evt.target.value || undefined;
-      if (nextValue !== state.current[param]) {
-        setState((prev) => {
-          let next = { ...prev };
-          //@ts-ignore
-          next.draft[param] = nextValue;
-          return next;
-        });
+      const nextValue = evt.target.value || undefined;
+      console.log({ nextValue });
+      const currentValue = state.current[param];
+      if (isNully(nextValue)) {
+        if (isNully(currentValue)) {
+          setState((prev) => {
+            let next = { ...prev };
+            delete next.draftDelta[param];
+            return next;
+          });
+        } else {
+          setState((prev) => {
+            let next = { ...prev };
+            next.draftDelta[param] = null;
+            return next;
+          });
+        }
       } else {
-        setState((prev) => {
-          let next = { ...prev };
-          delete next.draft[param];
-          return next;
-        });
+        if (nextValue !== currentValue) {
+          setState((prev) => {
+            let next = { ...prev };
+            (next.draftDelta[param] as any) = nextValue;
+            return next;
+          });
+        } else {
+          setState((prev) => {
+            let next = { ...prev };
+            delete next.draftDelta[param];
+            return next;
+          });
+        }
       }
     };
 
-  const makeSelect = (param: keyof AppComposersQuery["variables"]) => (
-    <select
-      value={state.current[param] || undefined}
-      onChange={onChange(param)}
-      test-id={`App-${param}-selector`}
-    >
-      <option value={undefined}></option>
-      {countries.enumValues.map((value, j) => (
-        <option value={value.name} key={j}>
-          {value.name}
-        </option>
-      ))}
-    </select>
-  );
+  function calcSelectValue(param: keyof AppComposersQuery["variables"]) {
+    let draft = state.draftDelta[param];
+
+    if (draft === null) {
+      return "";
+    }
+    if (draft) {
+      return draft;
+    }
+    return state.current[param] || "";
+  }
+
+  function shouldShowButtons() {
+    return Object.keys(state.draftDelta).filter((x) => x !== undefined).length > 0;
+  }
+
+  const makeSelect = (param: keyof AppComposersQuery["variables"]) => {
+    if ((data as any)[`${param}Values`]?.enumValues) {
+      console.log(param, calcSelectValue(param));
+      return (
+        <select
+          value={calcSelectValue(param)}
+          onChange={onChange(param)}
+          test-id={`App-${param}-selector`}
+        >
+          <option value={undefined}></option>
+          {(data as any)[`${param}Values`].enumValues.map((value: any, j: number) => (
+            <option value={value.name} key={j}>
+              {value.name}
+            </option>
+          ))}
+        </select>
+      );
+    }
+    return null;
+  };
   console.log(state);
   return (
     <div>
-      {countryValues?.enumValues && (
-        <select
-          value={state.current.country || undefined}
-          onChange={onChange("country")}
-          test-id="App-country-selector"
-        >
-          <option value={undefined}></option>
-          {countryValues.enumValues.map((value, j) => (
-            <option value={value.name} key={j}>
-              {value.name}
-            </option>
-          ))}
-        </select>
-      )}
-      {workKindValues?.enumValues && (
-        <select
-          value={state.current.workKind || undefined}
-          onChange={onChange("workKind")}
-          test-id="App-workKind-selector"
-        >
-          <option value={undefined}></option>
-          {workKindValues.enumValues.map((value, j) => (
-            <option value={value.name} key={j}>
-              {value.name}
-            </option>
-          ))}
-        </select>
-      )}
+      {makeSelect("country")}
+      {makeSelect("workKind")}
 
-      {Object.keys(state.draft).length > 0 && (
+      {shouldShowButtons() && (
         <div>
           <button onClick={handleApply}>apply</button>
           <button onClick={handleCancel}>cancel</button>
         </div>
       )}
+
       {composersQueryRef ? (
         <React.Suspense fallback={"Loading..."}>
           <ComposersListWrapper queryRef={composersQueryRef} />
         </React.Suspense>
       ) : (
-        <ComposersList composers={composers} />
+        <ComposersList composers={data.composers} />
       )}
     </div>
   );
